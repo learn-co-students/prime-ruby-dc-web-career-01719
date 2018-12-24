f2ef513fa97207b2b238fe99e1e908775c4a70c4
hai-nguyen1112 <hainguyen871112@gmail.com>
2018-12-24 13:59:37 -0500

    My prime commit

--- prime.rb
+++ prime.rb
@@ -1,1 +1,18 @@
-# Add  code here!
+def prime?(number)
+  if number < 2
+    return false
+  end
+  array_of_divisors = Array.new
+  i = 2
+  limit = number / 2
+  while i <= limit
+    array_of_divisors.push(i)
+    i += 1
+  end
+  array_of_divisors.each do |divisor|
+    if number % divisor == 0
+      return false
+    end
+  end
+  return true
+end
