5d3d89b79eae807f473164d062c1e37334e9a6b7
Ryan Gill <rdgill95@gmail.com>
2018-12-28 00:55:46 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
-# Add  code here!
+def prime?(int)
+ array = (2..int-1).to_a
+ prime = true
+  if int < 2 
+    prime = false
+  else
+   array.each do |x| 
+    if int % x == 0
+     prime = false
+     break
+    end
+   end
+  end
+
+ return prime
+end 
