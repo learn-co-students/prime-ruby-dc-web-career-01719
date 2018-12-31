01b892fdf94ef5bc3604951009409aaa10b314ae
smaraghi <maraghiserven@gmail.com>
2018-12-31 10:37:16 -0500

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
-# Add  code here!
+def prime?(int)
+    arr = (2..int-1).to_a
+    tick = true 
+    if int < 2
+      tick = false
+    else
+      arr.each do |num|
+        if int % num == 0 
+          tick = false
+        end
+      end
+    end
+    tick
+
+end
+
